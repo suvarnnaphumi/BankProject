a8bd1586f262e90a1aa859079cbff4c1f4f3af08
suvarnnaphumi <suvarnnaphumi.p@ku.th>
2026-09-23 00:32:55 +0700

    Update team_member_model.dart

--- lib/models/team_member_model.dart
+++ lib/models/team_member_model.dart
@@ -3,8 +3,6 @@ class TeamMemberModel {
   final String name;
   final String studentId;
   final int number;
-
-  /// ที่อยู่ไฟล์รูปโปรไฟล์ เช่น 'assets/images/member1.png' (ไม่ใส่ก็ได้)
   final String? imagePath;
 
   const TeamMemberModel({
